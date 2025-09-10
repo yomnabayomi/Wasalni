1489df4ade00528a66c3da189b41e82857e824b6
yomna <yomnaaymanbayomi@gmail.com>
2025-09-10 05:17:11 +0300

    implemented user functions

--- user.cpp
+++ user.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "user.h"
+
+user :: user (string name , string email , string telephone , string password){
+    this->name = name ;
+    this->email = email;
+    this->telephone = telephone ;
+    this->password =  password ;
+}
+
+//getters
+location user :: getCurrLocation(){
+    return this->curr_location;
+}
+
+location user :: getDestination(){
+    return this-> destination ;
+}
+
+//setters
+
+void user :: setCurrLocation(int x , int y , string name){
+    this -> curr_location = location({x,y},name);
+}
+
+
+void user :: setDestination(int x , int y , string name){
+    this -> destination= location({x,y},name);
+}
+
+
+void user :: requestRide(admin & Admin ){
+    string curr, dest ;
+    cout << "Enter the pick up and drop off locations ' names :" << l ;
+    cin >> curr >> dest ;
+    int firstID = Admin.findLocationByName(curr);
+    int secondID = Admin.findLocationByName(dest);
+    if(!(Admin.location_is_found(firstID) && Admin.location_is_found(secondID))){
+         cout << "One or both locations not found!" << endl;
+        return;
+    }
+    location current = Admin.locationById[firstID];
+    location destination = Admin.locationById[secondID];
+
+    // after implementing dijkstra we should call it with current and destination being the parameters 
+
+}
--- user.h
+++ user.h
@@ -2,17 +2,41 @@
 // Created by Hana on 9/2/2025.
 //
 
+
+
 #ifndef USER_H
 #define USER_H
 #include "admin.h"
 
 
 class user {
-location curr_location;
-location destination;
+
+
 string name;
 string email;
 string telephone;
+string password ;
+
+public :
+location curr_location = location({0,0}," ");
+location destination =location({0,0}," ");
+
+user (string , string , string , string);
+
+location getCurrLocation ();
+location getDestination();
+
+
+// how will the user enter his current location and destination 
+// setters are for admin/manager  not user frindly
+void setCurrLocation (int , int , string );
+void setDestination (int , int , string);
+
+
+void requestRide (admin&Admin);
+
+
+
 };
 
 
